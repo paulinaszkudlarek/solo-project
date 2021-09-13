441b6dac978f34bb057cb4d60a08bd88adb45264
Paulina Szkudlarek <p.szkudlarek94@gmail.com>
2021-09-13 14:08:41 +0200

    Add personal-data page

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,14 @@
 // settings
 const select = {
+  pages: '.page-wrapper',
   nav: '.nav',
   topbar: '.topbar-wrapper',
 };
 
 const classNames = {
+  active: {
+    page: 'active-page',
+  },
   nav: {
     active: 'nav-active',
   },
@@ -22,12 +26,28 @@ const app = {
     thisApp.topbar = document.querySelector(select.topbar);
 
 
-    thisApp.navigation.addEventListener('click', function() {
+    thisApp.navigation.addEventListener('click', function(e) {
+      e.preventDefault();
       thisApp.navigation.classList.toggle(classNames.nav.active);
-      thisApp.topbar.classList.toggle(classNames.topbar.active);
-      
-
+      thisApp.topbar.classList.toggle(classNames.topbar.active); 
     });
+  },
+
+  activePage: function(){
+    const thisApp = this;
+
+    thisApp.allPages = document.querySelectorAll(select.pages);
+
+    console.log('thisApp.allPages: ', thisApp.allPages);
+    
+    for(let page of thisApp.allPages) {
+      page.addEventListener('click', function(e) {
+        e.preventDefault();
+        page.classList.toggle(classNames.active.page); 
+      });
+    }
+    
+    
   },
 
   init: function () {

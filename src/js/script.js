a07130f849ac8b043db5f886ca380ed0481732ab
Paulina Szkudlarek <p.szkudlarek94@gmail.com>
2021-09-20 19:34:54 +0200

    Add html, style and js for login modal

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,11 @@
 // settings
 const select = {
-  pages: '.page-wrapper',
+  hamburger: 'hamburger',
+  pages: '.page',
+  pagesWrappers: '.page-wrapper',
   nav: '.nav',
   topbar: '.topbar-wrapper',
+  navLinks: '.nav-link',
 };
 
 const classNames = {
@@ -17,43 +20,97 @@ const classNames = {
   }
 };
 
+document.querySelector('form').addEventListener('submit', function(event) {
+  let isFormValidate = true;
+
+  const emailAddressInput = event.target.querySelector('input[name="email"]'); 
+
+  if(emailAddressInput.value.indexOf('@') < 0) {
+    isFormValidate = false;
+    emailAddressInput.parentElement.querySelector('.error').innerHTML = 'Błędny adres e-mail';
+  }
+
+  return !isFormValidate ? event.preventDefault() : true;
+});
+
+
 
 const app = {
   initNav: function (){
     const thisApp = this;
 
-    thisApp.navigation = document.querySelector(select.nav);
-    thisApp.topbar = document.querySelector(select.topbar);
+    thisApp.dom = [];
+    thisApp.dom.navigation = document.querySelector(select.nav);
+    thisApp.dom.topbar = document.querySelector(select.topbar);
 
+    const hamburger = document.getElementById(select.hamburger);
+    console.log(hamburger);
 
-    thisApp.navigation.addEventListener('click', function(e) {
+    hamburger.addEventListener('click', function(e) {
       e.preventDefault();
-      thisApp.navigation.classList.toggle(classNames.nav.active);
-      thisApp.topbar.classList.toggle(classNames.topbar.active); 
+      thisApp.dom.navigation.classList.toggle(classNames.nav.active);
+      thisApp.dom.topbar.classList.toggle(classNames.topbar.active); 
     });
   },
 
-  activePage: function(){
-    const thisApp = this;
+  // activePage: function(){
+  //   const thisApp = this;
 
-    thisApp.allPages = document.querySelectorAll(select.pages);
+  //   thisApp.allPages = document.querySelectorAll(select.pages);
+  //   thisApp.navLinks = document.querySelectorAll(select.navLinks);
+  //   thisApp.pagesWrappers = document.querySelectorAll(select.pagesWrappers);
 
-    console.log('thisApp.allPages: ', thisApp.allPages);
+  //   console.log('thisApp.navLinks: ', thisApp.navLinks);
+
+  //   for(let link of thisApp.navLinks) {
+  //     link.addEventListener('click', function(e) {
+  //       e.preventDefault();
+  //       console.log('e: ', e);
+
+  //       const linkId = e.target.id; nie mogę namierzyć id klikniętego linku, jeśli kliknę w ikonę lub napis to nie wyłapuje id 
+  //       console.log('linkId: ', linkId);
+
+  //     });
+  //   }
     
-    for(let page of thisApp.allPages) {
-      page.addEventListener('click', function(e) {
-        e.preventDefault();
-        page.classList.toggle(classNames.active.page); 
-      });
+    
+  // },
+
+  activeModal: function () {
+
+    function closeModal () {
+      document.getElementById('overlay').classList.remove('show');
     }
-    
-    
-  },
+
+    const closeByBtn = document.querySelectorAll('.js--close-modal').forEach(function(btn) {
+      btn.addEventListener('click', function(e) {
+        e.preventDefault();
+        closeModal();
+      });
+    });
+
+    const closeByOverlay = document.querySelector('#overlay').addEventListener('click', function(e) {
+      if(e.target === this) {
+        closeModal();
+      }
+    });
+
+    const closeByEsc = document.addEventListener('keyup', function(e) {
+      if(e.keyCode === 27) {
+        closeModal();
+      }
+    });
+ 
+    closeByBtn;
+    closeByOverlay;
+    closeByEsc;
+  }, 
 
   init: function () {
     const thisApp = this;
 
     thisApp.initNav();
+    thisApp.activeModal();
   },
 };
 
